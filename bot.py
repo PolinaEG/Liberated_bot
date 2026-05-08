9def62ed8ca670118f72394a0f0b8dee0c0edcdc
PolinaEG <49510133+PolinaEG@users.noreply.github.com>
2026-05-08 18:20:10 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -457,6 +457,7 @@ def main():
         app.run_webhook(
             listen="0.0.0.0",
             port=port,
+            url_path="/webhook",
             webhook_url=webhook_url,
             drop_pending_updates=True,
         )
